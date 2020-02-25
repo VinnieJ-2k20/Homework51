d7b88a433febdb2304d3ce7cc02c5b94064d5f87
VinnieJ-2k20 <60885495+VinnieJ-2k20@users.noreply.github.com>
2020-02-25 13:52:26 +0200

    Added folder creation and files counter

--- Homework51/src/com/gmail/ptimofejev/Main.java
+++ Homework51/src/com/gmail/ptimofejev/Main.java
@@ -7,22 +7,26 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		MyFileFilter mff1 = new MyFileFilter("txt");
+		MyFileFilter mff1 = new MyFileFilter("docx");
 		
 		File sourceFolder = new File("source");
 		File destinationFolder = new File("destination");
+		System.out.println(destinationFolder.mkdir());
 		
 		File[] toCopy = sourceFolder.listFiles(mff1);
+		int files = 0;
 		
 		for (File target : toCopy) {
 			File destination = new File(destinationFolder.getName() + "/" + target.getName());
+			
 			try {
 				FileService.fileCopy(target, destination);
+				files++;
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 		
+		System.out.println(files + " files successfully copied to destination folder.");
 	}
-
 }
